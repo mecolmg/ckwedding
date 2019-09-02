dce982411e3ec8d15dd1733fd44973b11a00c00d
mecolmg <mecolmg@vt.edu>
2019-09-02 16:08:52 -0400

    Cleanup: absolute paths and scss cleanup

--- src/HomeView/HomeView.js
+++ src/HomeView/HomeView.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./HomeView.module.scss";
-import Footer from "../Footer/Footer";
-import Navigation from "../Navigation/Navigation";
-import Splash from "../Splash/Splash";
-import WeddingInfo from "../WeddingInfo/WeddingInfo";
+import Footer from "Footer/Footer";
+import Navigation from "Navigation/Navigation";
+import Splash from "Splash/Splash";
+import WeddingInfo from "WeddingInfo/WeddingInfo";
 
 
 function HomeView() {
