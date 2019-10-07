6b64108bfe056806805761c008362127877e7661
Colm Gallagher <mecolmg@vt.edu>
2019-10-06 22:54:48 -0400

    Add links to Whitehall and Maps. Hide rehersal dinner info.

--- src/WeddingInfo/WeddingInfo.js
+++ src/WeddingInfo/WeddingInfo.js
@@ -4,25 +4,39 @@ import Countdown from "react-countdown-now";
 
 function WeddingInfo() {
   return (
-    <div className={styles.weddingInfo + ' card'}>
+    <div className={styles.weddingInfo + " card"}>
       <h1 className={styles.title}>The Wedding</h1>
       <div className={styles.countdown}>
         ~ <Countdown date="Sat, 26 Sep 2020" renderer={props => props.days} />{" "}
         Days Away ~
       </div>
       <div className={styles.events}>
-        <div className={styles.event}>
+        <div className={styles.event} hidden>
           <h2>The Rehersal Dinner</h2>
           <p>TBD</p>
         </div>
         <div className={styles.event}>
           <h2>The Reception</h2>
           <div>
-            <p>Whitehall Estate</p>
             <p>
-              18301 Whitehall Estate Ln,
-              <br />
-              Bluemont, VA 20135
+              <a
+                href="https://www.whitehallestate.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Whitehall Estate
+              </a>
+            </p>
+            <p>
+              <a
+                href="https://goo.gl/maps/X8tA6w9WLKUmvmzc7"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                18301 Whitehall Estate Ln,
+                <br />
+                Bluemont, VA 20135
+              </a>
             </p>
           </div>
         </div>
