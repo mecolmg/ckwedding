3b6b2854eca780892687c63b5155ce6e718d3821
skatel8 <skatel8@vt.edu>
2019-11-17 16:24:05 -0500

    Updates

---
+++ precache-manifest.5e6c96aee16e2c1b6be4d8cc2c387365.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a35537427a352bb1ed0dc06500147fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "758e27a5f5f4d4bb6938",
+    "url": "/static/css/main.91e165ab.chunk.css"
+  },
+  {
+    "revision": "4c11479d13a3b7ed7ad2",
+    "url": "/static/js/2.b174ce69.chunk.js"
+  },
+  {
+    "revision": "758e27a5f5f4d4bb6938",
+    "url": "/static/js/main.3732392b.chunk.js"
+  },
+  {
+    "revision": "94cd1e2f048bc0ae2bb3",
+    "url": "/static/js/runtime~main.47b9b368.js"
+  },
+  {
+    "revision": "8aa13dabcfc95977c8bf5d96a1f2a1f0",
+    "url": "/static/media/AcornStreet@0,25x.8aa13dab.jpg"
+  },
+  {
+    "revision": "e655ea7d2e962c2a9f0776c4db2d7556",
+    "url": "/static/media/AcornStreet@0,33x.e655ea7d.jpg"
+  },
+  {
+    "revision": "eae59cfa78f538869504d5d7a400a94d",
+    "url": "/static/media/AcornStreet@0,5x.eae59cfa.jpg"
+  },
+  {
+    "revision": "6ede344eb20471f15e4841a2582cafd2",
+    "url": "/static/media/ComfortSuites.6ede344e.png"
+  },
+  {
+    "revision": "eb47c2e435acf7dc2085d2cc8ecae8cc",
+    "url": "/static/media/MarriottLogo.eb47c2e4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fcc584ecac3f0708faa6bf59db7f9afa.js"
+  "/precache-manifest.5e6c96aee16e2c1b6be4d8cc2c387365.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
