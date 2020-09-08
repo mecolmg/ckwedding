1e5b302fb67d9fe12348e6c8a516ae8e61771bab
Colm Gallagher <mecolmg@gmail.com>
2020-09-08 01:57:43 -0400

    Updates

---
+++ precache-manifest.6d0dfd0cd99a0607858040bf6956c5a2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b04e169b17a9959d04ba5be400ad86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "069bb5c6d675f8ebe9e0",
+    "url": "/static/css/main.f12a4fad.chunk.css"
+  },
+  {
+    "revision": "87f4b1f0d7dd458144a0",
+    "url": "/static/js/2.ed49d873.chunk.js"
+  },
+  {
+    "revision": "2fb43a2616c582b35d6085b86b65ba98",
+    "url": "/static/js/2.ed49d873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "069bb5c6d675f8ebe9e0",
+    "url": "/static/js/main.b55b2563.chunk.js"
+  },
+  {
+    "revision": "2137111a8616fa131b63",
+    "url": "/static/js/runtime-main.2016aa4a.js"
+  },
+  {
+    "revision": "8aa13dabcfc95977c8bf5d96a1f2a1f0",
+    "url": "/static/media/AcornStreet@0,25x.8aa13dab.jpg"
+  },
+  {
+    "revision": "e655ea7d2e962c2a9f0776c4db2d7556",
+    "url": "/static/media/AcornStreet@0,33x.e655ea7d.jpg"
+  },
+  {
+    "revision": "eae59cfa78f538869504d5d7a400a94d",
+    "url": "/static/media/AcornStreet@0,5x.eae59cfa.jpg"
+  },
+  {
+    "revision": "6ede344eb20471f15e4841a2582cafd2",
+    "url": "/static/media/ComfortSuites.6ede344e.png"
+  },
+  {
+    "revision": "eb47c2e435acf7dc2085d2cc8ecae8cc",
+    "url": "/static/media/MarriottLogo.eb47c2e4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6cb12f9dfe8eb606c70b023a4d90c6ef.js"
+  "/precache-manifest.6d0dfd0cd99a0607858040bf6956c5a2.js"
 );
 
 self.addEventListener('message', (event) => {
