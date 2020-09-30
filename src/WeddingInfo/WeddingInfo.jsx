e839fbe69997b433ea103fba96d18deca35cf3c3
skatel8 <skatel8@vt.edu>
2020-09-30 16:44:50 -0400

    Added ceremony video and updated with past-tense verbage

--- src/WeddingInfo/WeddingInfo.jsx
+++ src/WeddingInfo/WeddingInfo.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import styles from './WeddingInfo.module.scss';
-import Countdown from 'react-countdown-now';
+import React from "react";
+import styles from "./WeddingInfo.module.scss";
+import Countdown from "react-countdown-now";
 
 function WeddingInfo(props) {
   const classNames =
-    styles.weddingInfo + (props.className ? ` ${props.className}` : '');
+    styles.weddingInfo + (props.className ? ` ${props.className}` : "");
   return (
     <div className={classNames}>
       <div className={styles.container}>
@@ -15,44 +15,33 @@ function WeddingInfo(props) {
         </h1>
         <p>
           With everything going on in the world today, we have made the decision
-          to postpone our wedding to{' '}
+          to postpone our wedding to{" "}
           <b>
             August 28<sup>th</sup>, 2021
           </b>
           , in hopes that those will be safer times. As you probably know
-          however, we have been engaged for longer than most, and will be{' '}
-          <i>tying the knot</i> on our original date - September 26
+          however, we have been engaged for longer than most, and decided to
+          <i>tie the knot</i> on our original date - September 26
           <sup>th</sup>, 2020 - witnessed only by our most immediate family.
         </p>
         <p>
           In an effort to simplify the task of sending out updates like this, we
           ask you please share your email with us in the form below.
         </p>
-        <p>
-          We know many of you may have made plans to attend already, and we
-          appologise for any inconveniences.
-        </p>
         <div className={styles.signature}>- Colm and Katie</div>
         <div className={styles.events}>
           <div className={styles.event}>
             <h2 className={styles.event}>The Private Ceremony</h2>
-            <div className={styles.countdown}>
-              ~{' '}
-              <Countdown
-                date="Sat, 26 Sep 2020"
-                renderer={(props) => props.days}
-              />{' '}
-              Days Away ~
-            </div>
+            <div className={styles.countdown}>~ Success! ~</div>
           </div>
           <div className={styles.event}>
-            <h2 className={styles.event}>Renewal of Vows {'&'} Reception</h2>
+            <h2 className={styles.event}>Renewal of Vows {"&"} Reception</h2>
             <div className={styles.countdown}>
-              ~{' '}
+              ~{" "}
               <Countdown
-                date="Sat, 28 Aug 2021"
-                renderer={(props) => props.days}
-              />{' '}
+                date="2021-08-28T17:00:00-05:00"
+                renderer={props => props.days}
+              />{" "}
               Days Away ~
             </div>
           </div>
@@ -78,6 +67,16 @@ function WeddingInfo(props) {
               Bluemont, VA 20135
             </a>
           </div>
+        </div>
+        <div className={styles.videoContainer}>
+          <iframe
+            className={styles.video}
+            src="https://www.youtube.com/embed/nInHhAkZYgY?rel=0"
+            frameborder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyros
+          cope; picture-in-picture; fullscreen"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
     </div>
