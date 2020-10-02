9e97268cfeedf13a2ad88a294c70a9e14af2be20
skatel8 <skatel8@vt.edu>
2020-10-02 16:19:46 -0400

    Pull gallery images from Firestore database.

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -1,23 +1,41 @@
-import React, {useEffect, useState} from 'react';
-import styles from './Gallery.module.scss';
-import {WEDDING_IMAGES} from './imagesData';
-import ImageGallery from 'react-image-gallery';
-import {isMobile} from 'react-device-detect';
+import React, { useEffect, useState } from "react";
+import styles from "./Gallery.module.scss";
+import {db} from "../firebase";
+import ImageGallery from "react-image-gallery";
+import Button from "@material-ui/core/Button";
+import { isMobile } from "react-device-detect";
 
 const Gallery = (props) => {
-  const images = WEDDING_IMAGES.map((image) => ({
-    original: `${image.url}=w1024`,
-    thumbnail: `${image.url}=w100`,
-  }));
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    db.collection('photos').get().then((snapshot) => {
+      setImages(snapshot.docs.map((doc) => {
+        const url = doc.get('url');
+        console.log(url);
+        return {
+          original: `${url}=w1024`,
+          thumbnail: `${url}=w100`,
+        };
+      }));
+    })
+  }, []);
 
   return (
     <div className={styles.gallery}>
       <ImageGallery
         items={images}
         autoPlay={true}
-        thumbnailPosition={isMobile ? 'bottom' : 'right'}
+        thumbnailPosition={isMobile ? "bottom" : "right"}
         lazyLoad={true}
       />
+      <Button
+        className={styles.photographerLink}
+        variant="contained"
+        color="primary"
+        href="https://birdsofafeatherphotography.passgallery.com/-kcmicrowedding/gallery"
+      >
+        View Photographer Gallery
+      </Button>
     </div>
   );
 };
