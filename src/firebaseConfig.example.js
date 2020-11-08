02b7a446e6629b8becbbc243c77176c1819fc37c
Colm Gallagher <mecolmg@gmail.com>
2020-11-08 14:25:48 -0500

    Add example firebase config file.

---
+++ src/firebaseConfig.example.js
@@ -0,0 +1,11 @@
+/** Values for these items can be found in the Firebase console. */
+export default {
+  apiKey: 'my-api-key',
+  authDomain: 'colm-and-katie-wedding.firebaseapp.com',
+  databaseURL: 'https://colm-and-katie-wedding.firebaseio.com',
+  projectId: 'colm-and-katie-wedding',
+  storageBucket: 'colm-and-katie-wedding.appspot.com',
+  messagingSenderId: 'message-sender-id',
+  appId: 'my-app-id',
+  measurementId: 'analytics-id',
+};
