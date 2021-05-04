2bdeaf27d389ae5bd9882ea51441815216015e3c
Colm Gallagher <mecolmg@gmail.com>
2021-05-03 22:39:04 -0400

    Enable skipping the meal section if no-one is attending.

--- src/RsvpForm/RsvpForm.jsx
+++ src/RsvpForm/RsvpForm.jsx
@@ -1,15 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import StepContent from '@material-ui/core/StepContent';
-import Button from '@material-ui/core/Button';
-import RespondentsStep from './RespondentsStep';
-import MealStep from './MealStep';
-import ContactStep from './ContactStep';
+import React, { useEffect, useState } from "react";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import StepContent from "@material-ui/core/StepContent";
+import Button from "@material-ui/core/Button";
+import RespondentsStep from "./RespondentsStep";
+import MealStep from "./MealStep";
+import ContactStep from "./ContactStep";
 
 const GET_ATTENDEES_URL =
-  'https://script.google.com/macros/s/AKfycbxTjLDFGXa9BtKJPKpWaISqkx2nhwHePKQzHE1o/exec';
+  "https://script.google.com/macros/s/AKfycbxTjLDFGXa9BtKJPKpWaISqkx2nhwHePKQzHE1o/exec";
 
 export default function RsvpForm() {
   const [activeStep, setActiveStep] = useState(0);
@@ -29,34 +29,34 @@ export default function RsvpForm() {
 
   const handleRespondentsChange = (newRespondents) => {
     setRespondents(
-      newRespondents.map(({...respondent}) => {
+      newRespondents.map(({ ...respondent }) => {
         if (!respondent.attending) {
-          respondent.mealChoice = '';
+          respondent.mealChoice = "";
           respondent.plusOneAttending = false;
         }
         if (!respondent.plusOneAttending) {
-          respondent.plusOneTitle = '';
-          respondent.plusOneName = '';
-          respondent.plusOneMealChoice = '';
+          respondent.plusOneTitle = "";
+          respondent.plusOneName = "";
+          respondent.plusOneMealChoice = "";
         }
         return respondent;
       })
     );
   };
 
-  const handleNext = () => {
+  const handleNext = (increment = 1) => {
     setActiveStep((prevActiveStep) => {
-      return prevActiveStep + 1;
+      return prevActiveStep + increment;
     });
   };
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  const handleBack = (increment = 1) => {
+    setActiveStep((prevActiveStep) => prevActiveStep - increment);
   };
 
   const getSteps = () => [
     {
-      label: 'Select Guest(s)',
+      label: "Select Guest(s)",
       content: (
         <RespondentsStep
           attendees={attendees}
@@ -67,12 +67,17 @@ export default function RsvpForm() {
       validate: () =>
         respondents.length > 0 &&
         respondents.filter(
-          ({plusOneAttending, plusOneName}) =>
+          ({ plusOneAttending, plusOneName }) =>
             !(plusOneAttending ^ !plusOneName)
         ).length === 0,
+      onNext: () => {
+        handleNext(
+          respondents.filter(({ attending }) => attending).length > 0 ? 1 : 2
+        );
+      },
     },
     {
-      label: 'Guest Meal(s)',
+      label: "Guest Meal(s)",
       content: (
         <MealStep
           respondents={respondents}
@@ -81,47 +86,62 @@ export default function RsvpForm() {
       ),
       validate: () =>
         respondents.filter(
-          ({attending, mealChoice, plusOneAttending, plusOneMealChoice}) =>
+          ({ attending, mealChoice, plusOneAttending, plusOneMealChoice }) =>
             !(attending ^ !mealChoice && plusOneAttending ^ !plusOneMealChoice)
         ).length === 0,
+      onNext: () => handleNext(),
+      onBack: () => handleBack(),
     },
     {
-      label: 'Contact Info',
+      label: "Contact Info",
       content: (
         <ContactStep
           respondents={respondents}
           onRespondentsChange={handleRespondentsChange}
         />
       ),
+      onNext: () => handleNext(),
+      onBack: () => {
+        handleBack(
+          respondents.filter(({ attending }) => attending).length > 0 ? 1 : 2
+        );
+      },
     },
   ];
 
   const steps = getSteps();
+  const isFinalStep = activeStep === steps.length - 1;
 
   return (
     <React.Fragment>
       <Stepper activeStep={activeStep} orientation="vertical">
-        {steps.map(({label, content, Content, validate}, index) => (
-          <Step key={index}>
-            <StepLabel>{label}</StepLabel>
-            <StepContent>
-              {content}
-              <div>
-                <Button disabled={activeStep === 0} onClick={handleBack}>
-                  Back
-                </Button>
-                <Button
-                  color="primary"
-                  disabled={validate ? !validate() : false}
-                  onClick={handleNext}
-                  variant="contained"
-                >
-                  {activeStep === steps.length - 1 ? 'Submit' : 'Next'}
-                </Button>
-              </div>
-            </StepContent>
-          </Step>
-        ))}
+        {steps.map(
+          ({ label, content, Content, validate, onBack, onNext }, index) => (
+            <Step key={index}>
+              <StepLabel>{label}</StepLabel>
+              <StepContent>
+                {content}
+                <div>
+                  {activeStep > 0 && <Button onClick={onBack}>Back</Button>}
+                  <Button
+                    color="primary"
+                    disabled={validate ? !validate() : false}
+                    onClick={
+                      isFinalStep
+                        ? () => {
+                            console.log(respondents);
+                          }
+                        : onNext
+                    }
+                    variant="contained"
+                  >
+                    {isFinalStep ? "Submit" : "Next"}
+                  </Button>
+                </div>
+              </StepContent>
+            </Step>
+          )
+        )}
       </Stepper>
       <Button disabled={activeStep === 0} onClick={handleBack}>
         Back
